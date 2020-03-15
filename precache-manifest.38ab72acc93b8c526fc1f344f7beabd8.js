471d0d74de6dd234656f3fe24893a4c5f56983a8
Jorge Rivera <jrdeveloper71@gmail.com>
2020-03-15 03:12:15 -0400

    Updates

---
+++ precache-manifest.38ab72acc93b8c526fc1f344f7beabd8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b7e6d790c5c62bda6cb5592fd4a271d",
+    "url": "/ping-pong-tournament/index.html"
+  },
+  {
+    "revision": "88c0e490457e591607ed",
+    "url": "/ping-pong-tournament/static/css/main.53d25195.chunk.css"
+  },
+  {
+    "revision": "d66ecc232b411019f582",
+    "url": "/ping-pong-tournament/static/js/2.60167ef2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/ping-pong-tournament/static/js/2.60167ef2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88c0e490457e591607ed",
+    "url": "/ping-pong-tournament/static/js/main.bc4a7c0c.chunk.js"
+  },
+  {
+    "revision": "21016cdf7d2810f19337",
+    "url": "/ping-pong-tournament/static/js/runtime-main.4e6f8ff2.js"
+  }
+]);
